cc918f7e90c21cd58483a8ec9bc5b1aac3482364
Matt Ragoza <mtr22@pitt.edu>
2021-09-12 01:32:38 -0400

    clean up and annotate

--- Puzzle8/bfs.py
+++ Puzzle8/bfs.py
@@ -8,7 +8,7 @@ vanilla breadth first search
 @author: milos
 """
 
-from Puzzle8 import *
+import Puzzle8
 
 
  #### ++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -16,51 +16,46 @@ from Puzzle8 import *
         
 
 def breadth_first_search(problem):
-     queue =deque([])
-     root=TreeNode(problem,problem.initial_state)
-     queue.append(root)   
-     while len(queue)>0:
-         next=queue.popleft()
-         if next.goalp():
-             del(queue)
-             return next.path()
-         else:
-             new_nodes=next.generate_new_tree_nodes()
-             for new_node in new_nodes:
-                  queue.append(new_node)         
-     print('No solution')
-     return NULL
+    queue = Puzzle8.deque([Puzzle8.TreeNode(problem, problem.initial_state)])
+    while queue:
+        curr = queue.popleft()
+        if curr.goalp():
+            return curr.path()
+        else:
+            queue.extend(curr.generate_new_tree_nodes())         
+    print('No solution')
+    return NULL
 
   
-problem=Puzzle8_Problem(Example1) 
-output=  breadth_first_search(problem)
+problem = Puzzle8.Puzzle8Problem(Puzzle8.Example1) 
+output = breadth_first_search(problem)
 print('Solution Example 1:')
-print_path(output)
+Puzzle8.print_path(output)
 
 wait = input("PRESS ENTER TO CONTINUE.")
 
-problem=Puzzle8_Problem(Example2) 
-output=  breadth_first_search(problem)
+problem = Puzzle8.Puzzle8Problem(Puzzle8.Example2) 
+output = breadth_first_search(problem)
 print('Solution Example 2:')
-print_path(output)
+Puzzle8.print_path(output)
 
 wait = input("PRESS ENTER TO CONTINUE.")
 
-problem=Puzzle8_Problem(Example3) 
-output=  breadth_first_search(problem)
+problem = Puzzle8.Puzzle8Problem(Puzzle8.Example3) 
+output = breadth_first_search(problem)
 print('Solution Example 3:')
-print_path(output)
+Puzzle8.print_path(output)
 
 wait = input("PRESS ENTER TO CONTINUE.")
 
-problem=Puzzle8_Problem(Example4) 
-output=  breadth_first_search(problem)
+problem = Puzzle8.Puzzle8Problem(Puzzle8.Example4) 
+output = breadth_first_search(problem)
 print('Solution Example 4:')
-print_path(output)
+Puzzle8.print_path(output)
 
 # Solution to Example 5 may take too long to calculate using vanilla bfs
-# problem=Puzzle8_Problem(Example5) 
-# output=  breadth_first_search(problem)
+# problem = Puzzle8.Puzzle8Problem(Puzzle8.Example5) 
+# output = breadth_first_search(problem)
 # print('Solution Example 5:')
-# print_path(output)
+# Puzzle8.print_path(output)
  
