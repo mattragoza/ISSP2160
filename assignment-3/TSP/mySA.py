38e75a5812772dad02713e9bc2b0562cd3c97f29
Matt Ragoza <mtr22@pitt.edu>
2021-09-28 11:13:30 -0400

    update mySA.py command line args

--- assignment-3/TSP/mySA.py
+++ assignment-3/TSP/mySA.py
@@ -1,15 +1,15 @@
-import math
+import sys, argparse, math
 import TSP, SA
 from SA import linear_cooling
 
 
-def cosine_cooling(i, k):
+def cosine_cooling(i, k, f=1):
     '''
     Return a cosine-modulated linear
     cooling coefficient for simulation 
     step i out of k.
     '''
-    return (k - i)/k * (1 + math.cos(math.pi*i)/2)
+    return (k - i)/k * (1 + math.cos(math.pi*i*f)/2)
 
 
 def power_cooling(i, k, b=0.01):
@@ -30,7 +30,24 @@ def step_cooling(i, k, b=0.5, n=5):
     return b**math.floor(n*i/k)
 
 
-if __name__ == '__main__':
+cooling_fn_map = {
+    'linear': linear_cooling,
+    'cosine': cosine_cooling,
+    'power': power_cooling,
+    'step': step_cooling,
+}
+
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n_steps', type=int, default=20000)
+    parser.add_argument('--init_temp', type=int, default=100)
+    parser.add_argument('--cooling_fn', default='linear')
+    return parser.parse_args(argv)
+
+
+def main(argv):
+    args = parse_args(argv)
     tsp_problem = TSP.TSP_Problem(TSP.Standard_Cities)
 
     n_trials = 10
@@ -44,8 +61,16 @@ if __name__ == '__main__':
             n_tours_tried,
             n_tours_accepted
         ) = SA.sim_anneal(
-            tsp_problem, 20000, 100, linear_cooling, verbose=False
+            tsp_problem,
+            no_of_steps=args.n_steps,
+            init_temperature=args.init_temp,
+            cooling_fn=cooling_fn_map[args.cooling_fn],
+            verbose=False
         )
         mean_dist += best_dist/n_trials
 
     print(f'mean_dist = {mean_dist:.2f}')
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
