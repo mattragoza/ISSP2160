85b52044d74184657597dc615960184c170c2d93
Matt Ragoza <mtr22@pitt.edu>
2021-09-30 21:48:01 -0400

    modify GA api for param search

--- assignment-4/TSP/GA.py
+++ assignment-4/TSP/GA.py
@@ -6,71 +6,92 @@ Genetic Algorithm for the TSP
 
 @authors: Milos Hauskrecht and Giacomo Nebbia
 """
+import sys, os, argparse, random
+import matplotlib.pyplot as plt
 
 from TSP import TSP_Problem, Standard_Cities
 from population import Population
-import random
-import os
-
-#parameters
-N_POP = 500 # population size
-NUM_GENERATIONS = 500 #number of generations to run
-MUTATION_PROB = 0.05 #probability of an individual being mutated
-CULLING_PERC = 0.05 #percentage of the least fit individuals to be removed
-ELITE_PERC = 0.05 #proportion of best individuals to carry over from one generation to the next
-OUT_DIR = 'fig/' #directory where to place the plots (it will be automatically created if not existent)
-
-#create output directory if not present
-if not os.path.isdir(OUT_DIR):
-    os.mkdir(OUT_DIR)
 
 
-#initialize the TSP problem
-problem=TSP_Problem(Standard_Cities)
+def parse_args(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n_population', default=500, type=int, help='population size')
+    parser.add_argument('--n_generations', default=500, type=int, help='number of generations to run')
+    parser.add_argument('--mutate_prob', default=0.05, type=float, help='probability of an individual being mutated')
+    parser.add_argument('--cull_pct', default=0.05, type=float, help='percentage of the least fit individuals to be removed')
+    parser.add_argument('--elite_pct', default=0.05, type=float, help='proportion of best individuals to carry over from one generation to the next')
+    parser.add_argument('--out_dir', default='fig/', help='directory where to place the plots (it will be automatically created if not existent')
+    parser.add_argument('--no_plot', default=False, action='store_true', help='output stats as a metrics file instead of a plot')
+    parser.add_argument('--out_file', default=None, help='name of file to output metrics')
+    return parser.parse_args(argv)
 
 
-#store generation-specific stats information
-fitness_vals = []
-min_fitness_vals = []
-pop_size = []
+def main(argv):
+    args = parse_args(argv)
 
-#generate initial population
-population = Population(problem, N_POP = N_POP)
+    # create output directory if not present
+    if not args.no_plot and not os.path.isdir(args.out_dir):
+        os.mkdir(args.out_dir)
 
-#store average and min fitess values for the current population
-fitness_vals.append(population.mean_fitness())
-min_fitness_vals.append(population.min_fitness())
-pop_size.append(len(population))  
-    
-# simulate for the MAX_ITERATION number of generations
-for curr_iter in range(NUM_GENERATIONS):
-    
-    # create a new population
-    population = population.build_a_new_generation(CULLING_PERC= CULLING_PERC, ELITE_PERC = ELITE_PERC, M_RATE=MUTATION_PROB)
-    
-    #store stats for the current population
+    # initialize the TSP problem
+    problem = TSP_Problem(Standard_Cities)
+
+    # store generation-specific stats information
+    fitness_vals = []
+    min_fitness_vals = []
+    pop_size = []
+
+    # generate initial population
+    population = Population(problem, N_POP=args.n_population)
+
+    # store average and min fitess values for the current population
     fitness_vals.append(population.mean_fitness())
-    best_fitness=population.min_fitness()
-    min_fitness_vals.append(best_fitness)
-    pop_size.append(len(population))
+    min_fitness_vals.append(population.min_fitness())
+    pop_size.append(len(population))  
     
-    # print stats for every 50th population
-    if curr_iter % 50 == 0:
-        print('Iteration [{}] of [{}] ({:.2f}%) -- Best solution: cost = [{:.2f}] -- Population size [{}]'.format(curr_iter, NUM_GENERATIONS, curr_iter * 100 / NUM_GENERATIONS, best_fitness, len(population)))   
-    
+    # simulate for the MAX_ITERATION number of generations
+    for curr_iter in range(args.n_generations):
+        
+        # create a new population
+        population = population.build_a_new_generation(
+            CULLING_PERC=args.cull_pct,
+            ELITE_PERC=args.elite_pct,
+            M_RATE=args.mutate_prob
+        )
 
-best_solution = population.get_best_individual()
-print("Best solution found has cost [{:.2f}]\n{}".format(best_solution.fitness, best_solution.tour))
+        # store stats for the current population
+        fitness_vals.append(population.mean_fitness())
+        best_fitness=population.min_fitness()
+        min_fitness_vals.append(best_fitness)
+        pop_size.append(len(population))
+        
+        # print stats for every 50th population
+        if curr_iter % 50 == 0:
+            print('Iteration [{}] of [{}] ({:.2f}%) -- Best solution: cost = [{:.2f}] -- Population size [{}]'.format(curr_iter, args.n_generations, curr_iter * 100 / args.n_generations, best_fitness, len(population)))   
 
- #plot results
-import matplotlib.pyplot as plt
-plt.plot(list(range(len(fitness_vals))), fitness_vals, label = 'Avg')
-plt.plot(list(range(len(fitness_vals))), min_fitness_vals, label = 'Min')
-plt.xlabel('Generations')
-plt.ylabel('Cost (opposite trend of fitness)')
-pars_as_str_title = 'N_pop = {}, p_mut = {:.2f}, p_cull={:.2f}, p_elite={:.2f}'.format(N_POP, MUTATION_PROB, CULLING_PERC, ELITE_PERC)
-plt.title('Evolution [{}]'.format(pars_as_str_title))
-plt.legend()
-pars_as_str_file = 'N_pop_{}_p_mut_{:02d}_p_cull_{:02d}_p_elite_{:02}'.format(N_POP, int(MUTATION_PROB * 100), int(CULLING_PERC * 100), int(ELITE_PERC * 100))
-plt.savefig(OUT_DIR + 'evolution_{}.png'.format(pars_as_str_file))
-plt.show()
+    if args.out_file:
+
+        with open(args.out_file, 'w') as f:
+            f.write('generation mean_fitness min_fitness pop_size\n')
+            for i, (mean_f, min_f, pop) in enumerate(zip(fitness_vals, min_fitness_vals, pop_size)):
+                f.write(f'{i} {mean_f} {min_f} {pop}\n')
+
+    if not args.no_plot:
+        best_solution = population.get_best_individual()
+        print("Best solution found has cost [{:.2f}]\n{}".format(best_solution.fitness, best_solution.tour))
+
+        # plot results
+        plt.plot(list(range(len(fitness_vals))), fitness_vals, label = 'Avg')
+        plt.plot(list(range(len(fitness_vals))), min_fitness_vals, label = 'Min')
+        plt.xlabel('Generations')
+        plt.ylabel('Cost (opposite trend of fitness)')
+        pars_as_str_title = 'N_pop = {}, p_mut = {:.2f}, p_cull={:.2f}, p_elite={:.2f}'.format(args.n_population, args.mutate_prob, args.cull_pct, args.elite_pct)
+        plt.title('Evolution [{}]'.format(pars_as_str_title))
+        plt.legend()
+        pars_as_str_file = 'N_pop_{}_p_mut_{:02d}_p_cull_{:02d}_p_elite_{:02}'.format(args.n_population, int(args.mutate_prob * 100), int(args.cull_pct * 100), int(args.elite_pct * 100))
+        plt.savefig(args.out_dir + 'evolution_{}.png'.format(pars_as_str_file))
+        plt.show()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
